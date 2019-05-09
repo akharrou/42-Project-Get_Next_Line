d59c7dd5ad32c7e5379b112c72eb638819130f76
Aymen Kh <idev.aymen@gmail.com>
2019-05-08 18:09:31 -0700

    Latest Update

---
+++ libft/ft_strnew.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akharrou <akharrou@student.42.us.org>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/19 07:21:53 by akharrou          #+#    #+#             */
+/*   Updated: 2019/05/08 18:01:44 by akharrou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strnew(size_t size)
+{
+	char *buf;
+
+	if (!(buf = ft_memalloc(size + 1)))
+		return (NULL);
+	return (buf);
+}
